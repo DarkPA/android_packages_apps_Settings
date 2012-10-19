5e8a6badc31b931a28539b9a130bae53969b2ee4
Jesús David <jedga_95@outlook.com>
2012-10-18 21:20:54 -0500

    Fix consistency
    Change-Id: I21377ac206db54030769801881fbf8c97db575cc

--- src/com/android/settings/cyanogenmod/NavigationBar.java
+++ src/com/android/settings/cyanogenmod/NavigationBar.java
@@ -104,7 +104,7 @@ public class NavigationBar extends SettingsPreferenceFragment implements OnPrefe
                     Integer.valueOf(String.valueOf(newValue)));
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SYSTEMUI_NAVBAR_COLOR, intHex);
+                    Settings.System.NAV_BAR_COLOR, intHex);
             return true;
         }
 
@@ -116,7 +116,7 @@ public class NavigationBar extends SettingsPreferenceFragment implements OnPrefe
         if (pref == mResetColor) {
             int color = 0xFF000000;
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SYSTEMUI_NAVBAR_COLOR, color);
+                    Settings.System.NAV_BAR_COLOR, color);
             mNavigationBarColor.onColorChanged(color);
         }
         return false;
